dc7de7b06844c758f9004710b9483b1acd12f74d
Alex K <alexander.kostadinov@alaricsecurities.com>
2021-03-14 00:32:09 +0200

    Reorganize files and add node-cli demo

---
+++ other/node-cli.js
@@ -0,0 +1,29 @@
+// main demo
+const chalk = require("chalk");
+const boxen = require("boxen");
+
+const greeting = chalk.white.bold("Hello!");
+
+const boxenOptions = {
+ padding: 1,
+ margin: 1,
+ borderStyle: "round",
+ borderColor: "green",
+ backgroundColor: "#555555"
+};
+const msgBox = boxen( greeting, boxenOptions );
+
+console.log(msgBox);
+
+
+// args
+const yargs = require("yargs");
+
+const options = yargs
+ .usage("Usage: -n <name>")
+ .option("n", { alias: "name", describe: "Your name", type: "string", demandOption: true })
+ .argv;
+
+const greeting = `Hello, ${options.name}!`;
+
+console.log(greeting);
